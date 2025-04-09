c2368d5dd841f64af214e2ed607fe78f2454345d
p_erik <penskyi.pn@ucu.edu.ua>
2025-04-09 09:10:54 +0300

    Update max_freq_stack.py
    added class FreqStack

--- Maximum Frequency Stack/max_freq_stack.py
+++ Maximum Frequency Stack/max_freq_stack.py
@@ -1,1 +1,22 @@
+from collections import defaultdict
+class FreqStack:
+
+    def __init__(self):
+        self.freq = defaultdict(int)
+        self.group = defaultdict(list)
+        self.max_freq = 0
+
+    def push(self, val: int) -> None:
+        self.freq[val] += 1
+        f = self.freq[val]
+        self.group[f].append(val)
+        if f > self.max_freq:
+            self.max_freq = f
+
+    def pop(self) -> int:
+        val = self.group[self.max_freq].pop()
+        self.freq[val] -= 1
+        if not self.group[self.max_freq]:
+            self.max_freq -= 1
+        return val
 
